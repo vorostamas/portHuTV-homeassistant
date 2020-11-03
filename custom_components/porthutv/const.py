38bb168347bf8e9a337fd19fd001177d781b7807
Tamas Voros <voros.e.tamas@gmail.com>
2020-11-03 22:58:42 +0000

    bump version

--- custom_components/porthutv/const.py
+++ custom_components/porthutv/const.py
@@ -3,7 +3,7 @@
 NAME = "PortHuTv"
 DOMAIN = "porthutv"
 DOMAIN_DATA = f"{DOMAIN}_data"
-VERSION = "1.0.2"
+VERSION = "1.0.3"
 
 ISSUE_URL = "https://github.com/vorostamas/portHuTV-homeassistant/issues"
 
