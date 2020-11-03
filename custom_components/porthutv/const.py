447cb07f23abb4be52976bfe2a60cda61478966c
Tamas Voros <voros.e.tamas@gmail.com>
2020-11-03 10:07:00 +0000

    remove sampleclient import

--- custom_components/porthutv/const.py
+++ custom_components/porthutv/const.py
@@ -3,7 +3,7 @@
 NAME = "PortHuTv"
 DOMAIN = "porthutv"
 DOMAIN_DATA = f"{DOMAIN}_data"
-VERSION = "1.0.1"
+VERSION = "1.0.2"
 
 ISSUE_URL = "https://github.com/vorostamas/portHuTV-homeassistant/issues"
 
